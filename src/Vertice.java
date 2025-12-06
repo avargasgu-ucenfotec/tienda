5d5bdcf831dd96d081604106a087cda08050620c
avargasgu-ucenfotec <avargasgu@ucenfotec.ac.cr>
2025-12-05 20:54:12 -0600

    Agrega clase Vertice

---
+++ src/Vertice.java
@@ -0,0 +1,24 @@
+class Vertice {
+
+    //Clase "Vertice" que representa una ubicación en un mapa.
+
+    //Atributos
+    private String nombre;  //Nombre de la ubicación
+    private int distancia;  //Distancia, en kilómetros, entre dos ubicaciones
+
+    //Métodos
+    //Constructor
+    public Vertice(String nombre, int distancia) {
+        this.nombre = nombre;
+        this.distancia = distancia;
+    }
+
+    //Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+}
